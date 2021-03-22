46e8b47b742f92d21c6ad0927204bc7d445bf309
Benjamin <work@benjaminrussell.me>
2021-03-22 15:12:17 -0500

    content data shape?

---
+++ store/reviews.js
@@ -0,0 +1,48 @@
+
+    export const state = () => {
+      reviews:[
+        {
+          title: 'Google Reviews',
+          reviews:[
+            {
+              content:'"Everybody at Hoglund made a hard situation so much easier! From the moment I called, to meeting with an attorney to figure out my options, all the way to my discharge hearing this morning, Hoglund has been with me every step of the way. They kept in constant communication, kept me informed of all the steps, and answered my questions. I can\'t say enough about how excellent my experience was with Hoglund. I highly recommend them if you need an attorney."',
+              author:'B.L.',
+            },
+            {
+              content:'"I have been so impressed by this law firm from day one. I am so grateful for the help and support that I received. Hoglund Law has gone above and beyond and exceeded my expectations. I highly recommend them. If I could summarize in one word: kindness. They are going to do everything they can to answer your questions and make sure you are prepared and informed and complete the paperwork thoroughly. Hoglund Law has shown me that lawyers can be great counselors. I felt supported by them at every turn. I believe they challenge the lawyer stigma: the lawyers at Hoglund are great people."',
+              author:'Matthew Hardy',
+            },
+            {
+              content:'"Hoglund Law firm was very helpful and friendly staff at all times. In addition, to there professionalism Hoglund worked hard, answered all of my questions at anytime and stayed on top of things in regards to my case. It was a pleasure working with Hoglund law firm and it\'s there pleasure to work for you."',
+              author:'Michelle Barnes Davenport',
+            }
+          ]
+        },
+        {
+          title: 'Our Testimonials',
+          reviews:[
+            {
+              content:'"They helped make a very difficult and overwhelming process easy. Staff is busy, but I have always been able to get someone on the phone the same day and many times right away. They were proactive in reaching out to me as needed and always were patient in answering my questions and concerns. I would highly recommend them to anyone if you are looking for an attorney\'s office who genuinely cares about their clients."',
+              author:'Gonzalo Curiel',
+            },
+            {
+              content:'"Love Hoglund law office. They took very good care of me. Always had time for my phone calls. If I had to leave a message they always got back with me in a timely manner. Awesome attorneys to work with. Highly recommend them!!!"',
+              author:'Brenda Millsl',
+            }
+          ]
+        },
+        {
+          title: 'Lawyers.com Reviews',
+          reviews:[
+            {
+              content:'"After going through a divorce after a 23 year marriage, I sought assistance from the Hoglund Law office for my bankruptcy. Robert and his team were professional, prompt and assisted me with everything needed to regain hope."',
+              author:'Past Client',
+            },
+            {
+              content:'"Andrew did a wonderful job with my daughter case. Very professional and explained everything in detail. This was second time for her Jennifer was our first attorney she was less professional and not very prepared or we would have won back then."',
+              author:'Past Client',
+            }
+          ]
+        },
+      ]
+    }
